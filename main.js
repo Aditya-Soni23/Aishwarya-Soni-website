c89c953a8d45d717b1c70b8cac1e4eeec21feed3
Aditya-Soni23 <77341618+Aditya-Soni23@users.noreply.github.com>
2021-07-17 15:05:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function one(){
+    window.alert("GARDEN")
+}
+
+function two(){
+    window.alert("THE QUEEN")
+}
+
+function three(){
+    window.alert("ROLE MODEL")
+}
+
+function four(){
+    window.alert("STYLING")
+}
+
+function five(){
+    window.alert("BIRTHDAY")
+}
+
+function six(){
+    window.alert("ANCIENT STONE")
+}
+
+function seven(){
+    window.alert("SMILE")
+}
